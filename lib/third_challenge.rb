63fd63580a09b34fa15dfb4c8574015e0ae5c8f0
Abideen Kareem <abideenkareem17@gmail.com>
2019-06-10 21:31:50 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
         :age => "15"
         :status => "alive"
         },
-      :hero_friends => []
+      :hero_friends => [{:name : },{}]
    }, 
    :capulet => {
       :patriarch => {
